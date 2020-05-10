cbbbc248e84a20037067e03a4fcc8257a46d1cd6
Amir Czeiger <amircz1997@gmail.com>
2020-05-10 12:40:02 +0300

    complex calculator finished!

--- scripts.js
+++ scripts.js
@@ -1,153 +1,221 @@
-var operator = "";
-var arg1 = "";
-var arg2 = "";
-var opButtons = document.querySelectorAll(".op")
-var numButtons = document.querySelectorAll(".num");
-var expression = document.getElementById("expression");
-var numberBox = document.getElementById("numberBox");
-var equal = document.getElementById("equal");
-var hasAnswer = false;
-var hasPoint = false;
-var answer;
-var hasMinus = false;
-function addListenersToOps() {
-    for (var i = 0; i < opButtons.length; i++)
-        opButtons[i].addEventListener("click", function () {
-            if (numberBox.value == "" && this.textContent == "-") {
-                numberBox.value += "-";
-                hasMinus = true;
-            }
-            else {
-                if (operator == "" || hasAnswer) {
-                    operator = this.textContent;
-                    if (!hasAnswer)
-                        arg1 = numberBox.value;
-                    else
-                        arg1 = answer;
-                    expression.textContent = arg1 + "\n" + operator;
-                    numberBox.value = "";
-                }
-            }
-            hasPoint = false;
-        })
+const expression = document.querySelector(".expression");
+const numberBox = document.querySelector(".numberBox");
+let answer;
+let complexExpHelper = "";
+let expressionIndex = 0;
+let expressionStack = [];
+const allOperators = ["+", "-", "*", "/"];
+let operationsObjectGlobal = {
+    "+": (first, second) => { return first + second; },
+    "-": (first, second) => { return first - second; },
+    "*": (first, second) => { return first * second; },
+    "/": (first, second) => {
+        if (second == "0") {
+            expression.textContent = "ERROR - DIVIDE BY 0";
+            clearAll(false);
+            return "";
+        }
+        return first / second;
+    }
+};
+
+clearAll(true);
+
+function operatorClicked(event) {
+    if (!allOperators.includes(numberBox.value[numberBox.value.length - 1])) {
+        if (answer != "z") {
+            expression.textContent = "";
+            numberBox.value += answer;
+            answer = "z";
+        }
+        if (numberBox.value == "" && event.target.textContent != "-") {
+            numberBox.value += "0";
+        }
+        numberBox.value += event.target.textContent;
+    }
 }
 
-function addListenersToEuqal() {
-    equal.addEventListener("click", function () {
-        if (!arg1 == "") {
-            arg2 = numberBox.value;
-            expression.textContent += arg2 + "=";
-            numberBox.value = "";
-            calculate();
+function setOperatorAndFirstArgument(op) {
+    operator = op;
+    if (answer == "z") {
+        if (numberBox.value == "" || numberBox.value == "-") {
+            firstArgument = "0";
         }
         else {
-            alert("error! please enter only numbers or negetive numbers to the box, not operrators. enter only one argument and use the operators buttons")
-            clearAll();
+            firstArgument = numberBox.value;
         }
-    })
-}
-
-function addListenersToNums() {
-    for (var i = 0; i < numButtons.length; i++)
-        numButtons[i].addEventListener("click", function () {
-            numberBox.value = addCharToTheEnd(numberBox.value, this.textContent);
-        })
-}
-function addListenerToDel() {
-    var delButton = document.getElementById("del");
-    delButton.addEventListener("click", function () {
-        if (numberBox.value[numberBox.value.length - 1] == ".")
-            hasPoint = false;
-        numberBox.value = removeLastChar(numberBox.value);
-    })
-}
-function addListenerToClear() {
-    var clearButton = document.getElementById("clear");
-    clearButton.addEventListener("click", function () {
-        clearAll();
-    })
-}
-function addListenerToPoint() {
-    var pointButton = document.getElementById("point");
-    pointButton.addEventListener("click", function () {
-        if (!hasPoint) {
-            numberBox.value += ".";
-            hasPoint = true;
-        }
-    })
-}
-function clearAll() {
+    }
+    else {
+        firstArgument = answer;
+    }
+    expression.textContent = firstArgument + operator;
     numberBox.value = "";
-    expression.textContent = "";
-    operator = "";
-    hasAnswer = false;
-    arg1 = "";
-    arg2 = "";
-    hasPoint = false;
-    hasMinus = false;
 }
 
-function calculate() {
-    hasAnswer = true;
-    hasPoint = false;
-    if (Number.isInteger(arg1))
-        arg1 = parseInt(arg1);
-    else
-        arg1 = parseFloat(arg1);
+function setOperatorAndExpression(op) {
+    operator = op;
+    expression.textContent = firstArgument + operator;
+}
 
-    if (Number.isInteger(arg2))
-        arg2 = parseInt(arg2);
-    else
-        arg2 = parseFloat(arg2);
+function minusClicked(event) {
+    operatorClicked(event);
+}
 
-    switch (operator) {
-        case "+": {
-            expression.textContent += (arg1 + arg2);
-            answer = arg1 + arg2
+function equalClicked(event) {
+    if (numberBox.value == "") {
+        answer = 0;
+        expression.textContent = "0";
+    }
+    else {
+        complexExpHelper = "";
+        expression.textContent = numberBox.value;
+        createHelper();
+        addExpressionToStackAndCalculatePriority(complexExpHelper);
+        calculateAndUpdateExpression();
+        numberBox.value = "";
+    }
+}
+
+function numbersclicked(event) {
+    numberBox.value = numberBox.value.concat(event.target.textContent);
+}
+
+function deleteClicked(event) {
+    numberBox.value = numberBox.value.slice(0, numberBox.value.length - 1);
+}
+
+function pointClicked() {
+    if (numberBox.value[numberBox.value.length - 1] != ".") {
+        if (numberBox.value == "") {
+            numberBox.value += "0.";
         }
-            break;
-        case "-": {
-            expression.textContent += arg1 - arg2;
-            answer = arg1 - arg2;
+        else {
+            numberBox.value += ".";
         }
-            break;
-        case "*": {
-            expression.textContent += arg1 * arg2;
-            answer = arg1 * arg2;
+    }
+}
+
+function addListenerToKeyBoard() {
+    document.addEventListener("keydown", event => {
+        let plusButton = document.getElementById("plus");
+        let minusButton = document.getElementById("minus");
+        let multButton = document.getElementById("mult");
+        let divideButton = document.getElementById("divide");
+        let equalButton = document.getElementById("equal");
+        let pointButon = document.getElementById("point");
+        let operationsObject = {
+            "187": plusButton,
+            "189": minusButton,
+            "56": multButton,
+            "191": divideButton,
+            "13": equalButton,
+            "190": pointButon
+        };
+        //equal case
+        if (event.keyCode == "13") {
+            operationsObject[event.keyCode].click();
         }
-            break;
-        case "/": {
-            if (arg2 != 0) {
-                expression.textContent += arg1 / arg2;
-                answer = arg1 / arg2;
+        else {
+            if (event.keyCode in operationsObject) {
+                operationsObject[event.keyCode].click();
+                if (document.activeElement == numberBox) {
+                    numberBox.value = numberBox.value.slice(0, numberBox.value.length - 1);
+                }
             }
             else {
-                alert("ERROR - CANNOT DIVIDE BY 0")
-                clearAll();
+                //digit case ascii between 48 and 57
+                if (event.keyCode >= "0".charCodeAt(0) && event.keyCode <= "9".charCodeAt(0) &&
+                    document.activeElement != numberBox) {
+                    numberBox.value += String.fromCharCode(event.keyCode);
+                }
             }
-
         }
-            break;
-    }
-    arg1 = "";
+    })
 }
+
+function createHelper() {
+    for (let boxIndex = 0; boxIndex < numberBox.value.length; boxIndex++) {
+        if (allOperators.includes(numberBox.value[boxIndex])) {
+            //if not starting with negetive number
+            if (boxIndex != 0 || numberBox.value[boxIndex] != "-") {
+                complexExpHelper += ",";
+            }
+            complexExpHelper += numberBox.value[boxIndex];
+            complexExpHelper += ",";
+        }
+        else {
+            complexExpHelper += numberBox.value[boxIndex];
+        }
+    }
+}
+
+function addExpressionToStackAndCalculatePriority(expression) {
+    let tempArgument;
+    let tempOp;
+    let argumentsList = [];
+    argumentsList = expression.split(",");
+    let isFirst = true;
+    while (argumentsList.length != 0) {
+        tempArgument = argumentsList.shift();
+        if (isFirst && tempArgument == "-") {
+            tempArgument += argumentsList.shift();
+        }
+        if (tempArgument != "*" && tempArgument != "/") {
+            if (tempArgument != "-" && tempArgument != "+") {
+                tempArgument = Number.parseFloat(tempArgument);
+            }
+            expressionStack.push(tempArgument);
+        }
+        else {
+            expressionStack.push(operationsObjectGlobal[tempArgument](expressionStack.pop(), argumentsList.shift()));
+        }
+        isFirst = false;
+    }
+}
+
+function calculateAndUpdateExpression() {
+    let tempFirst;
+    let tempSecond;
+    let tempOp;
+    while (expressionStack.length != 1) {
+        tempFirst = expressionStack.shift();
+        tempOp = expressionStack.shift();
+        tempSecond = expressionStack.shift();
+        expressionStack.unshift(operationsObjectGlobal[tempOp](tempFirst, tempSecond));
+    }
+    answer = expressionStack.pop();
+    expression.textContent += "=" + answer;
+    expressionStack = [];
+
+}
+
+function clearAll(toClearExpression) {
+    if (toClearExpression) {
+        expression.textContent = "";
+    }
+    //used z because answer="" is the same as answer=0, which is optional case
+    answer = "z";
+    numberBox.value = "";
+    complexExpHelper = "";
+    expressionStack.push(0);
+    expressionStack.push("+");
+}
+
 function addAllListeners() {
-    addListenersToOps();
-    addListenersToEuqal();
-    addListenersToNums();
-    addListenerToDel();
-    addListenerToClear();
-    addListenerToPoint();
-}
-function removeLastChar(str) {
-    result = "";
-    for (var i = 0; i < str.length - 1; i++) {
-        result += str[i];
+    let allButtonsListeners = {
+        "num": numbersclicked,
+        "clear": clearAll,
+        "equal": equalClicked,
+        "del": deleteClicked,
+        "op": operatorClicked,
+        "minus": minusClicked,
+        "point": pointClicked
     }
-    return result;
-}
-function addCharToTheEnd(str, ch) {
-    return str.concat(ch);
+
+    let allButtons = document.getElementsByTagName("button");
+    Array.from(allButtons).forEach(element => element.onclick = allButtonsListeners[element.className]);
+
 }
 
 addAllListeners();
+addListenerToKeyBoard();
